32da37e3b56836a14d5f85c1ce0edc85521a69c9
Jimmy Liang <paradoxical.reasoning@gmail.com>
2012-02-14 22:38:15 -0500

    Changed the build system to use cabal

--- Makefile
+++ Makefile
@@ -1,26 +1,23 @@
-all: target alloyIG.jar
-	ghc -XDeriveDataTypeable -isrc src/Main.hs -outputdir target --make -o claferIG
+all: install
 
-target:
-	@if test -n "$(shell cabal info haxml | grep 'Latest version installed' | grep 'Not installed')"; then \
-		echo "[ERROR] Missing haxml library. Try 'cabal install haxml'."; false; \
-	fi
-	@if test -n "$(shell cabal info cmdargs | grep 'Latest version installed' | grep 'Not installed')"; then \
-		echo "[ERROR] Missing cmdargs library. Try 'cabal install cmdargs'."; false; \
-	fi
-	@if test -n "$(shell cabal info executable-path | grep 'Latest version installed' | grep 'Not installed')"; then \
-		echo "[ERROR] Missing executable-path library. Try 'cabal install executable-path'."; false; \
-	fi
+install: dist/javabuild alloyIG.jar
+	cabal install --bindir=.
+	
+# Build takes less time. For ease of development.
+build: dist/javabuild alloyIG.jar
+	ghc -XDeriveDataTypeable -isrc src/Main.hs -outputdir dist/build --make -o claferIG
+
+dist/javabuild:
 	@if test ! -f "alloy4.jar"; then \
 		echo "[ERROR] Missing alloy4.jar. Try copying the jar into the current directory."; false; \
 	fi
-	mkdir -p target
+	mkdir -p dist/javabuild
 
 alloyIG.jar: src/manifest src/org/clafer/ig/AlloyInterface.java src/edu/mit/csail/sdg/alloy4compiler/parser/AlloyCompiler.java
-	javac -cp "alloy4.jar" -d target src/org/clafer/ig/AlloyInterface.java src/edu/mit/csail/sdg/alloy4compiler/parser/AlloyCompiler.java
-	jar cfm alloyIG.jar src/manifest -C target org/clafer/ig/ -C target edu
+	javac -cp "alloy4.jar" -d dist/javabuild src/org/clafer/ig/AlloyInterface.java src/edu/mit/csail/sdg/alloy4compiler/parser/AlloyCompiler.java
+	jar cfm alloyIG.jar src/manifest -C dist/javabuild org/clafer/ig/ -C dist/javabuild edu
 	
 clean:
-	rm -rf target
+	rm -rf dist
 	rm -f alloyIG.jar
 	rm -f claferIG
--- src/ClaferModel.hs
+++ src/ClaferModel.hs
@@ -1,3 +1,25 @@
+{-
+ Copyright (C) 2012 Jimmy Liang <http://gsd.uwaterloo.ca>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+-}
+
 module ClaferModel (ClaferModel, Clafer(..), buildClaferModel) where
 
 import Data.List 
@@ -60,17 +82,19 @@ buildSigMap (Solution sigs _) = Map.fromList $ zip (map s_label sigs) sigs
 -- A map of label -> ID
 buildTypeMap :: Solution -> Map String Int
 buildTypeMap (Solution sigs fields) =
-    buildTuples tuples
+    mapSigs `Map.union` mapTuples
     where
     buildSig :: Sig -> Map String Int
     buildSig sig = Map.fromList $ zip (map a_label $ s_atoms sig) (repeat $ s_id sig)
     
-    tuples = concatMap f_tuples fields
     buildTuples :: [Tuple] -> Map String Int
-    buildTuples [] = foldr (Map.union) Map.empty (map buildSig sigs)
+    buildTuples [] = Map.empty
     buildTuples (t:ts) =
         Map.insert (t_from t) (t_fromType t) $
         Map.insert (t_to t) (t_toType t) (buildTuples ts)
+        
+    mapSigs = foldr (Map.union) Map.empty (map buildSig sigs)
+    mapTuples = buildTuples $ concatMap f_tuples fields
 
 
 buildClaferModel :: Solution -> ClaferModel
