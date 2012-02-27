a74966cc59c9f00954556a488a64f08a94781822
Jimmy Liang <paradoxical.reasoning@gmail.com>
2012-02-27 18:59:24 -0500

    Fixed http://gsd.uwaterloo.ca:8888/question/162/scope-handling-unknown-clafer-name

--- src/Main.hs
+++ src/Main.hs
@@ -23,20 +23,28 @@
 module Main where
 
 import ClaferIG
+import ClaferModel
 import CommandLine
+import Control.Monad
+import Data.Maybe
+import Data.IORef
 import Prelude hiding (all)
 import System.Console.CmdArgs
+import System.Directory
+import System.FilePath
 
 
 data IGArgs = IGArgs {
     all :: Maybe Int,
+    saveDir :: Maybe FilePath,  
     claferModelFile :: FilePath
 } deriving (Show, Data, Typeable)
 
 
 
 claferIG = IGArgs {
-    all             = def &= help "Saves all instances up to the provided scope or a counterexample." &= name "all",
+    all             = def &= help "Saves all instances up to the provided scope or a counterexample.",
+    saveDir         = def &= help "Specify the directory for storing saved files." &= typ "FILE",
     claferModelFile = def &= argPos 0 &= typ "FILE"
 } &= summary claferIGVersion
 
@@ -52,27 +60,40 @@ main =
                     setGlobalScope scope claferIG
                     solve claferIG
                     
-                    let saveNames = map (saveName $ claferModelFile args) [1,2..]
-                    saveAll saveNames claferIG
+                    let file = claferModelFile args
+                    counterRef <- newIORef 1
+                    
+                    let saveDirectory = fromMaybe return $ underDirectory `liftM` saveDir args
+                    saveAll (savePath file counterRef >>= saveDirectory) claferIG
                     return ()
             Nothing    -> runCommandLine claferIG
             
         quit claferIG
-        
-        
-saveName :: FilePath -> Int -> FilePath
-saveName file counter = file ++ "." ++ (show counter) ++ ".data"
 
 
-saveAll :: [FilePath] -> ClaferIG -> IO [FilePath]
-saveAll [] _ = fail "No more filepaths"
-saveAll (f:fs) claferIG =
+savePath :: FilePath -> IORef Int -> IO FilePath
+savePath file counterRef =
     do
+        counter <- readIORef counterRef
+        writeIORef counterRef (counter + 1)
+        return $ file ++ "." ++ (show counter) ++ ".data"
+        
+
+underDirectory :: FilePath -> FilePath -> IO FilePath
+underDirectory dir file =
+    do
+        createDirectoryIfMissing True dir
+        return $ joinPath [dir, file]
+
+
+saveAll :: IO FilePath -> ClaferIG -> IO ()
+saveAll nextFile claferIG =
+    do
+        file <- nextFile
         solution <- next claferIG
         case solution of
             Just nextSolution ->
                 do
-                    writeFile f (show nextSolution)
-                    saved <- saveAll fs claferIG
-                    return $ f:saved
-            Nothing           -> return []
+                    writeFile file (show nextSolution)
+                    saveAll nextFile claferIG
+            Nothing           -> return ()
