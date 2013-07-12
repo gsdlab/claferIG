65fe6231b1d41cbac53ed7fcfe321b8a0a10a0c4
Luke Brown <Luke.michael.brown@gmail.com>
2013-07-12 10:41:49 -0400

    Fixed a bug where sometimes UnSAT was not shown when calling claferModel in certain cases
    Also Moved backquoted as it has yet to be fixed

--- src/Language/Clafer/IG/CommandLine.hs
+++ src/Language/Clafer/IG/CommandLine.hs
@@ -332,7 +332,7 @@ runCommandLine =
                     if (num `elem` cLines && isEmptyLine l) then editLines cLines unSATs m1 m2 s lineMap rest else (show num ++ "." ++ (replicate (1 + m1 - (numberOfDigits $ fromIntegral num)) ' ') ++ (if (isUnSAT unSATs l num) then "> " else "| ") ++ l ++ (replicate (3 + m2 - (length l)) ' ') ++ (if (isUnSAT unSATs l num) then "<UnSAT " else "|      ") ++ (addScopeVal s l (Map.lookup num lineMap))) : editLines cLines unSATs m1 m2 s lineMap rest
 
                 isUnSAT :: [String] -> String -> Integer -> Bool
-                isUnSAT us l ln = getAny $ foldMap (\u -> Any ((u `isInfixOf` l) || ("column" `isInfixOf` u && "line" `isInfixOf` u && (init $ head $ tail $ tail $ reverse $ words u) == show ln))) us
+                isUnSAT us l ln = getAny $ foldMap (\u -> Any ((and $ map (`elem` (words l)) (words u)) || ("column" `isInfixOf` u && "line" `isInfixOf` u && (init $ head $ tail $ tail $ reverse $ words u) == show ln))) us
 
                 addScopeVal :: [(String, Integer)] -> String -> (Maybe String) ->String
                 addScopeVal s l Nothing = ""
